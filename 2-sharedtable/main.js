9a0794f0a633349b6903e05d96a01492765e0e75
Kai Ninomiya <kainino@chromium.org>
2022-10-28 17:58:36 -0700

    fixup sharedtable test

--- 2-sharedtable/main.js
+++ 2-sharedtable/main.js
@@ -6,11 +6,11 @@ const device = await adapter.requestDevice();
 const buffer = device.createBuffer({ size: 8, usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ });
 
 const table = new GPUSharedTable();
+console.log('inserting into table', device, buffer);
 table.insert(1, device);
 table.insert(2, buffer);
-const sab = new SharedArrayBuffer();
 
-console.log('sending', table, sab);
+console.log('sending', table);
 
 worker.onmessage = async () => {
   await buffer.mapAsync(GPUMapMode.READ);
@@ -18,7 +18,7 @@ worker.onmessage = async () => {
   console.log(new Uint32Array(mapped));
 };
 
-worker.postMessage({ table, sab });
+worker.postMessage({ table });
 device.queue.writeBuffer(buffer, 4, new Uint32Array([77777]));
 
 
--- 2-sharedtable/worker.js
+++ 2-sharedtable/worker.js
@@ -1,11 +1,12 @@
 globalThis.onmessage = ev => {
   console.log('received', ev.data);
   if (ev.data !== 'unavailable') {
-    const { table, sab } = ev.data;
-    console.log('received', table, sab);
+    const { table } = ev.data;
+    console.log('received', table);
 
     const device = table.get(1);
     const buffer = table.get(2);
+    console.log('retrieved from table', device, buffer);
 
     device.queue.writeBuffer(buffer, 0, new Uint32Array([33333]));
   }
